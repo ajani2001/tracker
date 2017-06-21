8bae485f672718594fd08a4045635e1ffed4b025
ajani2001 <ivanovgrigoriinikolaevich00@gmail.com>
2017-06-21 19:58:02 +0700

    replace basic google map implementation by  special package, prepare to implement the connection to the remote server, upload local files to work at the Granny's

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,19 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { DDP } from 'meteor/ddp-client';
+import { ReactiveDict } from 'meteor/reactive-dict'
 import './body.html';
 
 import { location } from '../api/gps.js';
 
+Meteor.startup( function(){
+    this.clientStorage= new ReactiveDict();
+    DDP.connect('http://90.188.118.184:3000');
+    GoogleMaps.load();
+});
+
 Template.body.helpers({
-    currentPosition(){
-        var oneRecord = location.findOne();
-        if (oneRecord === undefined) {
+    currentPosition: function(){
+        var lastRecord = location.find({ location: { $ne: null } }, { sort: { time: -1 } } ).fetch()[0];
+        if (lastRecord === undefined) {
             return;
         }
-        if ('errorInfo' in oneRecord) {
-            alert(oneRecord.errorInfo.message);
+        if ('errorInfo' in lastRecord) {
+            alert(lastRecord.errorInfo.message);
         }
-        return 'latitude: ' + oneRecord.location.coords.latitude + ' longitude: ' + oneRecord.location.coords.longitude;
+        return 'latitude: ' + lastRecord.location.coords.latitude + ' longitude: ' + lastRecord.location.coords.longitude + ' time: ' + lastRecord.time;
+    },
+    mapOnLoad: function(){
+        if(GoogleMaps.loaded()){
+            return {
+                center: {lat: 0, lng: 0},
+                zoom: 1
+            };
+        }
     },
 });
 
@@ -31,11 +47,23 @@ Template.body.events({
             return toReturn;
         }
         function success(position) {
+            //if()
             Meteor.call('location.saveCurrent', clone(position));
         }
         function error(info) {
+            alert(info.message);
             Meteor.call('location.saveError', clone(info));
         }
         navigator.geolocation.getCurrentPosition(success, error);
+
+        var lastRecord = location.find({ location: { $ne: null } }, { sort: { time: -1 } } ).fetch()[0];
+        var position = { lat: lastRecord.location.coords.latitude, lng: lastRecord.location.coords.longitude };
+        var map = GoogleMaps.maps.display.instance;
+        var marker = new google.maps.Marker({
+            position,
+            map
+        });
+        map.setZoom(8);
+        map.setCenter(position);
     },
 });
